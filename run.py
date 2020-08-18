fb5e8ff5d446f5f25746af962b82daa063fa4346
Jamie Davis <jamjam@umich.edu>
2020-08-18 10:30:16 -0400

    Added --loop option to run.py

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
+import sys
+import time
+
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    if "--loop" in sys.argv:
+        while True:
+            app.run()
+            print("Sleeping...")
+            time.sleep(180)
+    else:
+        app.run()
